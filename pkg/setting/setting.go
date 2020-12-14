26a795fb6c3cd9c29ee5d85b8618e4eb9e15ea7d
Teemu Harju <tsharju@aiven.io>
2020-12-14 16:27:21 +0200

    Settings: add config options to disable config variable expansion

--- pkg/setting/setting.go
+++ pkg/setting/setting.go
@@ -261,8 +261,10 @@ type Cfg struct {
 	// CSPEnabled toggles Content Security Policy support.
 	CSPEnabled bool
 	// CSPTemplate contains the Content Security Policy template.
-	CSPTemplate           string
-	AngularSupportEnabled bool
+	CSPTemplate                  string
+	AngularSupportEnabled        bool
+	DisableEnvVariableExpansion  bool
+	DisableFileVariableExpansion bool
 
 	TempDataLifetime                 time.Duration
 	PluginsEnableAlpha               bool
@@ -765,6 +767,18 @@ func (cfg *Cfg) loadConfiguration(args CommandLineArgs) (*ini.File, error) {
 
 	// apply command line overrides
 	applyCommandLineProperties(commandLineProps, parsedFile)
+
+	// enable or disable env and file variable expansion
+	if security := parsedFile.Section("security"); security != nil {
+		cfg.DisableEnvVariableExpansion = security.Key("disable_env_variable_expansion").MustBool(false)
+		cfg.DisableFileVariableExpansion = security.Key("disable_file_variable_expansion").MustBool(false)
+	}
+	if cfg.DisableEnvVariableExpansion {
+		removeExpander("env")
+	}
+	if cfg.DisableFileVariableExpansion {
+		removeExpander("file")
+	}
 
 	// evaluate config values containing environment variables
 	err = expandConfig(parsedFile)
